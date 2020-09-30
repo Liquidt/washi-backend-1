ebd2416660b94f825e7ceeba8bba7de6a980c2e7
Flavio Saavedra <f.saavedra1810@gmail.com>
2020-09-30 16:00:54 -0500

    Adding PaymentMethod

---
+++ Washi.API/Domain/Repositories/IPaymentMethodRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Washi.API.Domain.Models;
+
+namespace Washi.API.Domain.Repositories
+{
+    public interface IPaymentMethodRepository
+    {
+        Task<IEnumerable<PaymentMethod>> ListAsync();
+        Task AddSync(PaymentMethod user);
+        Task<PaymentMethod> FindById(int id);
+        void Update(PaymentMethod user);
+        void Remove(PaymentMethod user);
+    }
+}
